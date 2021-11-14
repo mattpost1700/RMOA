cb827092e5c8017f7d8ce78744e3be6b2d249ada
Ryan Wakefield <rlwakefield88@gmail.com>
2021-11-14 16:01:40 -0500

    Implemented half of FoodDrinkView

--- src/components/mainContentContainer/FoodDrinkView.js
+++ src/components/mainContentContainer/FoodDrinkView.js
@@ -1,10 +1,28 @@
 import React from "react";
+import FoodDrinkSubView from "./FoodDrinkSubView";
 
 class FoodDrinkView extends React.Component{
-
+    handleBackToOrderViewClicked = () =>{
+        console.log("handleBackToOrderViewClicked is clicked!");
+        this.props.backToOrderViewProps();
+    }
     render(){
         return(
-            <p>I'm a FoodDrinkView!</p>
+            <div>
+                <button 
+                id="backToOrderView"
+                onClick={() => this.handleBackToOrderViewClicked()}
+                >Back to Orders</button>
+                <div id="bill">
+                <FoodDrinkSubView 
+                orderItemsProps={this.props.billModelProps.orderItems}
+                removeOrderItemProps={this.props.removeOrderItemProps}
+                />
+                </div>
+                <div id="menu">
+
+                </div>
+            </div>
         )
     }
 }
