49604a11e8a31be3acc5358502ca4f5f9cd21323
Cory Sauder <corysauder@gmail.com>
2021-11-19 10:35:04 -0500

    Finished styling FoodDrinkView

--- src/components/mainContentContainer/FoodDrinkView.js
+++ src/components/mainContentContainer/FoodDrinkView.js
@@ -5,6 +5,7 @@ import "../../appCSS/food-drink-view.css";
 class FoodDrinkView extends React.Component{
     state = {
         mSwitch: -1,
+        addClick: false,
     }
     handleBackToOrderViewClicked = () =>{
         console.log("handleBackToOrderViewClicked is clicked!");
@@ -15,9 +16,15 @@ class FoodDrinkView extends React.Component{
         this.setState({mSwitch: this.state.mSwitch * -1})
     }
 
+    addClicked = () => {
+        this.setState({addClick: true})
+    }
 
-    scrollToBottom = () => {
-        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.state.addClick) {
+            this.messagesEnd.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            this.setState({addClick: false});
+        }
     }
 
     render(){
@@ -28,28 +35,33 @@ class FoodDrinkView extends React.Component{
                 <button className={"food-drink__back"}
                     id="backToOrderView"
                     onClick={() => this.handleBackToOrderViewClicked()}
-                >Back to Orders</button>
+                >&#60;&#60; Back to Orders</button>
                 <div className="food-drink__wrapper">
-                <div id="bill" className={"food-drink__bill"}>
+                    <div className="food-drink__bill-wrapper">
+                        <div id="bill" className={"food-drink__bill"}>
 
-                    <p className={"food-drink__bill-num"}>Bill #{morderItemsProps[0].bill}</p>
-                    <FoodDrinkSubView
-                        orderItemsProps={morderItemsProps}
-                        billModelProps={this.props.billModelProps}
-                        removeOrderItemProps={this.props.removeOrderItemProps}
-                        modifyItemsCallbackProps={this.modifyItemsCallback}
-                    />
-                    <div ref={(el) => {this.messagesEnd = el;}}/>
-                </div>
-                <div id="menu" className={"food-drink__menu"}>
-                    <MenuView
-                        orderItemsProps={morderItemsProps}
-                        billModelProps={this.props.billModelProps}
-                        addOrderItemsProps={this.props.addOrderItemsProps}
-                        modifyItemsCallbackProps={this.modifyItemsCallback}
-                        scrollToBottom={this.scrollToBottom}
-                    />
-                </div>
+                            <p className={"food-drink__bill-num"}>Bill #{morderItemsProps[0].bill}</p>
+                            <FoodDrinkSubView
+                                orderItemsProps={morderItemsProps}
+                                billModelProps={this.props.billModelProps}
+                                removeOrderItemProps={this.props.removeOrderItemProps}
+                                modifyItemsCallbackProps={this.modifyItemsCallback}
+                            />
+                            <div className={"food-drink__scroll"} ref={(el) => {this.messagesEnd = el;}}/>
+                        </div>
+                    </div>
+                    <div className="food-drink__menu-wrapper">
+                        <div id="menu" className={"food-drink__menu"}>
+                            <p className={"food-drink__title"}>Menu</p>
+                            <MenuView
+                                orderItemsProps={morderItemsProps}
+                                billModelProps={this.props.billModelProps}
+                                addOrderItemsProps={this.props.addOrderItemsProps}
+                                modifyItemsCallbackProps={this.modifyItemsCallback}
+                                addClickProps={this.addClicked}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
