03dcf6546bf8e2d51d5017c4917a967ead03f2e1
Ryan Wakefield <rlwakefield88@gmail.com>
2021-11-21 15:41:03 -0500

    Started to refactor OrderView.js with Dictionary data structure

--- src/components/mainContentContainer/OrderView.js
+++ src/components/mainContentContainer/OrderView.js
@@ -5,6 +5,7 @@ import OrderItem from "../OrderItem"
 import OrderSubView from "./OrderSubView";
 import FoodDrinkView from "./FoodDrinkView";
 import SplitBillView from "./SplitBillView";
+import Dictionary from "../dataStructures/Dictionary";
 // Not sure if this is a good idea or not, but the idea is
 // to have this class represent the OrderModel
 
@@ -53,7 +54,57 @@ class OrderView extends React.Component{
         console.log("mCheckboxes", mCheckboxes);
         return mCheckboxes;
     }
+    setCheckboxesRefactor = () =>{
+        console.log("In setCheckBoxes");
+        let numOfBills = this.props.orderProps.order.totalBills;
+        console.log("numOfBills", numOfBills);
+        let mCheckboxes = new Dictionary();
+        let billNumbers = this.generateOrderSubViewRefactor().getAllKeys();
+        for(let i = 0; i < billNumbers.length; i++){
+            //All billNumbers should be unique
+            mCheckboxes.addKeyPair(billNumbers[i],{checked:false});
+        }
+        console.log("mCheckboxes", mCheckboxes);
+        return mCheckboxes;
+    }
     handleCheckboxClick = (mIndex) =>{
+        //console.log("hello from handleCheckboxClick method");
+        //console.log("index is", index);
+        
+        let flag = this.state.checkboxes[mIndex];
+        //console.log("flag is", flag);
+        if(flag === true){
+            let temp = this.state.checkboxes;
+            for(let i = 0; i < temp.length; i++){
+                if(i === mIndex){
+                    temp[i] = false;
+                }
+            }
+            this.setState({checkboxes: temp})
+        }
+        else if(flag === false){
+            let count = 0;
+            let flags = this.state.checkboxes;
+            for(let i = 0; i < flags.length; i++){
+                if(flags[i] === true){
+                    //console.log("flags[i] was ", flags[i]);
+                    count = count + 1;
+                }
+            }
+            if(count < 2){
+                //console.log("count is ", count);
+                let temp = this.state.checkboxes;
+                for(let i = 0; i < temp.length; i++){
+                    if(i === mIndex){
+                        temp[i] = true;
+                    }
+                }
+                this.setState({checkboxes: temp})
+            }
+        }
+        this.setState({checkboxes: this.state.checkboxes});
+    }
+    handleCheckboxClickRefactor = (mIndex) =>{
         //console.log("hello from handleCheckboxClick method");
         //console.log("index is", index);
         
@@ -111,14 +162,15 @@ class OrderView extends React.Component{
         if(firstBill !== 0 && secondBill === 0){
             //set the BillModel
             //Cannot use generateOrderSubView()
-            let mOrderItems = this.getConfirmedOrderItemsBills()[firstBill - 1]; //Handle off by 1
-            console.log("mOrderItems", mOrderItems);
+            //let mOrderItems = this.getConfirmedOrderItemsBills()[firstBill - 1]; //Handle off by 1
+            let mOrderItemsR = this.getConfirmedOrderItemsBillsRefactor().getValueOfKey(firstBill);
+            console.log("mOrderItems", mOrderItemsR);
             this.setState(
                 {firstBillModel: {
                     orderID: this.props.orderProps.tableID,
                     paid: false,
                     bill: firstBill,
-                    orderItems: mOrderItems,
+                    orderItems: mOrderItemsR,
                 }
                 }
             )
@@ -170,13 +222,14 @@ class OrderView extends React.Component{
             //set the BillModel
             //Cannot use generateOrderSubView()
             let mOrderItems = this.getConfirmedOrderItemsBills()[firstBill - 1]; //Handle off by 1
-            console.log("mOrderItems", mOrderItems);
+            let mOrderItemsR = this.getConfirmedOrderItemsBillsRefactor().getValueOfKey(firstBill);
+            console.log("mOrderItems", mOrderItemsR);
             this.setState(
                 {firstBillModel: {
                     orderID: this.props.orderProps.tableID,
                     paid: false,
                     bill: firstBill,
-                    orderItems: mOrderItems,
+                    orderItems: mOrderItemsR,
                 }
                 }
             )
@@ -260,7 +313,8 @@ class OrderView extends React.Component{
     generateMainContentView = () =>{
         switch(this.state.mainContent.name){
             case "Main OrderView":{
-                let mBillsArray = this.generateOrderSubView();
+                //let mBillsArray = this.generateOrderSubView();
+                let mBillsDict = this.generateOrderSubViewRefactor();
             return(
                 <div className={"orderView"}>
                     <div id="infoContainer" className={"orderView__info"}>
@@ -285,7 +339,10 @@ class OrderView extends React.Component{
                         </div>
                         <p className={"orderView__orderNum"}>Order # {this.props.orderProps.order.orderID}</p>
                     </div>
-                    {this.generateBills(mBillsArray)}
+                    {//Refactored
+                    //this.generateBills(mBillsArray)
+                    this.generateBillsRefactor(mBillsDict)
+                    }
                 </div>
             )
             }
@@ -343,6 +400,36 @@ class OrderView extends React.Component{
         }
         return billsArray
     }
+    // This function takes the order and separates
+    // it into different bills. billsDict is a new Dictionary data structure
+    generateOrderSubViewRefactor = () =>{
+        let billsDict = new Dictionary();
+        
+        //These are the confirmed OrderItems
+        let mOrderItems = this.props.orderProps.order.orderItems;
+        for(let i = 0; i < mOrderItems.length; i++){
+            if(!billsDict.isKey(mOrderItems[i].bill)){
+                billsDict.addKeyPair(mOrderItems[i].bill, []);
+                billsDict.addArrayElement(mOrderItems[i].bill, mOrderItems[i]);
+            }
+            else{
+                billsDict.addArrayElement(mOrderItems[i].bill, mOrderItems[i]);
+            }
+        }
+        //These are the temporary OrderItems
+        let mTempOrderItems = this.state.tempOrderItems;
+        for(let i = 0; i < mTempOrderItems.length; i++){
+            if(!billsDict.isKey(mTempOrderItems[i].bill)){
+                billsDict.addKeyPair(mTempOrderItems[i].bill, []);
+                billsDict.addArrayElement(mTempOrderItems[i].bill, mTempOrderItems[i]);
+            }
+            else{
+                billsDict.addArrayElement(mTempOrderItems[i].bill, mTempOrderItems[i]);
+            }
+        }
+        
+        return billsDict
+    }
     getConfirmedOrderItemsBills = () =>{
         let billsArray = [];
         let numOfBills = this.props.orderProps.order.totalBills;
@@ -359,6 +446,22 @@ class OrderView extends React.Component{
         }
         return billsArray
     }
+    getConfirmedOrderItemsBillsRefactor = () =>{
+        let billsDict = new Dictionary();
+        
+        //These are the confirmed OrderItems
+        let mOrderItems = this.props.orderProps.order.orderItems;
+        for(let i = 0; i < mOrderItems.length; i++){
+            if(!billsDict.isKey(mOrderItems[i].bill)){
+                billsDict.addKeyPair(mOrderItems[i].bill, []);
+                billsDict.addArrayElement(mOrderItems[i].bill, mOrderItems[i]);
+            }
+            else{
+                billsDict.addArrayElement(mOrderItems[i].bill, mOrderItems[i]);
+            }
+        }
+        return billsDict;
+    }
     getTempOrderItemsBills = () =>{
         let billsArray = [];
         let numOfBills = this.props.orderProps.order.totalBills;
@@ -374,7 +477,22 @@ class OrderView extends React.Component{
         }
         return billsArray
     }
-
+    getTempOrderItemsBillsRefactor = () =>{
+        let billsDict = new Dictionary();
+        //These are the temporary OrderItems
+        let mTempOrderItems = this.state.tempOrderItems;
+        for(let i = 0; i < mTempOrderItems.length; i++){
+            if(!billsDict.isKey(mTempOrderItems[i].bill)){
+                billsDict.addKeyPair(mTempOrderItems[i].bill, []);
+                billsDict.addArrayElement(mTempOrderItems[i].bill, mTempOrderItems[i]);
+            }
+            else{
+                billsDict.addArrayElement(mTempOrderItems[i].bill, mTempOrderItems[i]);
+            }
+        }
+        
+        return billsDict
+    }
 
 
     //This function breaks the bills down if they are too large
@@ -454,7 +572,91 @@ class OrderView extends React.Component{
         );
 
     }
+    //This function breaks the bills down if they are too large
+    //This does not affect the actual data structure of a bill
+    generateBillsRefactor = (mBillsDict) =>{
+        //number of items per sub bill
+        let numPerBill = 7;
 
+        let billGroup = [];
+        let bills = [];
+        let olNum; //starting number for ordered list
+        //loop through each bill
+        // Instead of mBillsArray.length, use getAllKeys
+        let billNumbers = mBillsDict.getAllKeys()
+        for(let idx = 0; idx < billNumbers.length; idx++) {
+            billGroup = [];
+
+            //split the items in the bill into pages with length of numPerBill
+            // Instead of mBillsArray[idx], use getValueOfKey, which will be an array in this case
+            let subGroupOfSeven = [];
+            let mBill = mBillsDict.getValueOfKey(billNumbers[idx]);
+            for (let i = 0; i < mBill.length; i++) { 
+                //i.e. if this next element will be the 7th or last element
+                // push to billGroup once added, then clear subGroupOfSeven for next subGroup
+                if( ((i + 1) % numPerBill === 0) || (i + 1) === mBill.length){
+                    subGroupOfSeven.push(mBill[i]);
+                    billGroup.push(subGroupOfSeven);
+                    subGroupOfSeven = [];
+                }
+                else{
+                    subGroupOfSeven.push(mBill[i]);
+                }
+                
+            }
+            //billGroup should still be a 2D array at this point
+
+
+            let page = [];
+
+            let active = 'orderView__active';
+            //loop for each page of the bill
+            for (let i = 0; i < billGroup.length; i++) {
+                olNum = i * numPerBill + 1;
+                if(i > 0){
+                    active = 'orderView__inactive';
+                }
+                page.push(
+                    <div className={"orderView__order-subwrapper " + active} id={"bill"+billGroup[i][0].bill + "-page" + i}>
+
+                        <div className={"orderView__order"}
+                             key={idx}
+                        >
+                            <p className={"orderView__billnum"}>Bill # {billGroup[i][0].bill}</p>
+                            <OrderSubView
+                                billProps={billGroup[i]}
+                                olStart={olNum}
+                            />
+                        </div>
+                    </div>
+                )
+            }
+
+
+            //add the order wrapper to the bill pages
+            bills.push(<div className={"orderView__order-wrapper"} id={"bill" + billNumbers[idx]}>
+
+                <input
+                    className={"orderView__selection"}
+                    type="checkbox"
+                    checked={this.state.checkboxes.getValueOfKey(billNumbers[idx]).checked}
+                    onChange={() => this.handleCheckboxClickRefactor(billNumbers[idx])}
+                />
+                {page}
+     
+                 <button className={"orderView__next"} onClick={() => {this.cycleBill(billNumbers[idx])}}>Next</button>
+                
+
+            </div>);
+        }
+
+        return(
+            <div id="orderSubContainer" className={"orderView__container no-anim"}>
+                {bills}
+            </div>
+        );
+
+    }
     //Change bill page
     cycleBill = (bill) => {
         //prevent animation on page load
