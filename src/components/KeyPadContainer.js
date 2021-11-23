6b597a949592eb4acc52cb1ff0b5b1b45e32ef6a
Matt <mvp5782@psu.edu>
2021-11-23 18:04:32 -0500

    Added docstrings/organized

--- src/components/KeyPadContainer.js
+++ src/components/KeyPadContainer.js
@@ -4,16 +4,27 @@ import EnterClearContainer from "./EnterClearContainer";
 import "../appCSS/key-pad-container.css";
 
 import MainScreenOverlay from "./MainScreenOverlay";
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import {collection, getDocs, query, where} from 'firebase/firestore';
 
+/**
+ * The log in screen with the pin pad container
+ */
 class KeyPadContainer extends React.Component {
-    state ={
+    /**
+     * The pin number state
+     * @type {{pin: string, pinLength: number}}
+     */
+    state = {
         pin: "",
         pinLength: 0 // for some reason "str.length" was returning undefined before
     }
 
+    /**
+     * The action when the a keypad button is clicked
+     * @param button The UI reference to the js button
+     */
     handleButtonClick = (button) => {
-        if(this.state.pinLength < 4) {
+        if (this.state.pinLength < 4) {
             this.setState({
                 pin: this.state.pin + button,
                 pinLength: this.state.pinLength + 1
@@ -23,6 +34,9 @@ class KeyPadContainer extends React.Component {
         }
     }
 
+    /**
+     * The action when the "Clear" button is clicked
+     */
     handleClearClick = () => {
         this.setState({
             pin: "",
@@ -30,6 +44,10 @@ class KeyPadContainer extends React.Component {
         })
     }
 
+    /**
+     * The action when the "Submit" button is clicked
+     * @returns {Promise<void>}
+     */
     handleSubmitClick = async () => {
         console.log("handleSubmitClick", ": ", "started")
         const q = query(collection(this.props.dbProps, "users"), where("pin", "==", this.state.pin))
@@ -46,18 +64,19 @@ class KeyPadContainer extends React.Component {
 
             console.log("handleSubmitClick", ": ", fName + " " + lName, "logged in")
 
+            // TODO: Add routing to respective screens shown in ($...)
             switch (position) {
                 case 'server':
-                    // Go to main screen
+                    // Go to main screen ($MainScreenOverlay)
                     break;
                 case 'manager':
-                    // Go to manager screen
+                    // Go to manager screen ($MainScreenOverlay)
                     break;
                 case 'host':
-                    // Go to host screen
+                    // Go to host screen ($MainScreenOverlay)
                     break;
                 case 'kitchen':
-                    // Gp to kitchen view
+                    // Gp to kitchen view ($'KitchenOverlay') NOT CREATED YET
                     break;
             }
         });
@@ -69,24 +88,27 @@ class KeyPadContainer extends React.Component {
         })
 
         // check if logged in
-        if(!loggedIn) {
+        if (!loggedIn) {
             console.log("handleSubmitClick", ": ", "PIN does not match any user");
         }
     }
 
-
-    render(){
-        return(
+    /**
+     * Renders the page
+     * @returns {JSX.Element}
+     */
+    render() {
+        return (
             <div className={"login"}>
                 <h1 className={"login__title"}>Restaurant Login</h1>
                 <div className="login__content">
                     <div className={"login__details"}>
                         <label htmlFor="empPin">Pin</label>
                         <input readOnly
-                            className={"login__pin"}
-                            id="empPin"
-                            type="password"
-                            value={this.state.pin}
+                               className={"login__pin"}
+                               id="empPin"
+                               type="password"
+                               value={this.state.pin}
                         />
                     </div>
                     <div className={"keypad-buttons"}>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import reactDOM from "react-dom";
 import "./index.css";
 import KeyPadContainer from "./components/KeyPadContainer";
 
+// Firestore
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+/**
+ * Initialized connection to our firestore
+ * @type {Firestore}
+ */
+const db = getFirestore(initializeApp({
     apiKey: "AIzaSyDfcxJYmzZFj1C8RWTUaDWEpR3njO6-Knc",
     authDomain: "rmoa-77360.firebaseapp.com",
     projectId: "rmoa-77360",
@@ -14,11 +19,11 @@ const firebaseConfig = {
     messagingSenderId: "382803345424",
     appId: "1:382803345424:web:bf7889d15373f26c944a95",
     measurementId: "G-7HS8R7V1YV"
-};
+}));
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+/**
+ * Renders start screen
+ */
 reactDOM.render(
     <React.StrictMode>
         <KeyPadContainer dbProps={db}/>
