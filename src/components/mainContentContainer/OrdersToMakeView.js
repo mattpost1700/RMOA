cc5cfdecc01363284a2171d4d64308404bd7542b
mattpost1700 <72893809+mattpost1700@users.noreply.github.com>
2021-12-04 03:00:21 -0500

    Not working

--- src/components/mainContentContainer/OrdersToMakeView.js
+++ src/components/mainContentContainer/OrdersToMakeView.js
@@ -1,10 +1,17 @@
 import React from "react";
-import {collection, getDocs, query } from "firebase/firestore";
+import {collection, getDocs, query} from "firebase/firestore";
 
 class OrdersToMakeView extends React.Component {
     state = {
         orders: [],
         test: ""
+    }
+
+    handleRefreshClick = async () => {
+        console.log("handleRefreshClick", ": ", "clicked!")
+        await this.getOrders()
+        console.log("handleRefreshClick", ": ", "updated!")
+        console.log("handleRefreshClick", ": ", this.state.orders)
     }
 
     getOrders = async () => {
@@ -15,23 +22,38 @@ class OrdersToMakeView extends React.Component {
         console.log("getOrders", ": ", "starting query...")
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
+            //console.log("getOrders", ": ", "document found!")
             this.state.orders.push(doc.get("orderItems"))
             //this.setState(test = doc.get("orderItems"))
 
-            if(items === "") {
+            if (items === "") {
                 items = doc.get("orderItems")
-            } else { items += "\n" + doc.get("orderItems") }
+            } else {
+                items += "\n" + doc.get("orderItems")
+            }
 
             tableId = doc.get("tableID")
         });
+        return this.state.orders
     }
 
-    render(){
+
+    // this.state.orders.map((item, i) => <li key={i}>Test</li>
+// {this.state.orders.map((order, index) => (
+// <p>{index}: {order}</p>
+// ))}
+    render() {
         this.getOrders()
-        return(
-            <p>k{this.state.orders}</p>
+        return (
+            <div>
+                <ul>
+                    {this.state.orders.map((item, i) => <li key={i}>Test</li>}
+                </ul>
+            </div>
+
         )
     }
 
 }
+
 export default OrdersToMakeView
