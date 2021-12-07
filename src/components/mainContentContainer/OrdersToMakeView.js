7407095b6012d1e29ffc64b75acb3e1a04fee1db
mattpost1700 <72893809+mattpost1700@users.noreply.github.com>
2021-12-07 00:59:27 -0500

    Fixed variable name issue

--- src/components/mainContentContainer/OrdersToMakeView.js
+++ src/components/mainContentContainer/OrdersToMakeView.js
@@ -1,11 +1,85 @@
 import React from "react";
+import {collection, getDocs, query, where, orderBy} from "firebase/firestore";
+import {forEach} from "react-bootstrap/ElementChildren";
 
-class OrdersToMakeView extends React.Component{
-    render(){
-        return(
-            <p>Order To Make will appear here!</p>
+//import OrdersToMakeSubView from "./OrdersToMakeSubView";
+
+class OrdersToMakeView extends React.Component {
+
+    state = {
+        orders: [],
+        test: ""
+    }
+
+    handleRefreshClick = async () => {
+        console.log("handleRefreshClick", ": ", "clicked!")
+        await this.getOrders()
+        console.log("handleRefreshClick", ": ", "updated!")
+        console.log("handleRefreshClick", ": ", this.state.orders)
+    }
+
+    getOrders = async () => {
+        console.log("getOrders", ": ", "started")
+        const q = query(collection(this.props.dbProps, "orders"), where("completed", "==", false), orderBy("createdAt")) // oldest ticket is higher
+        //let tempString = "";
+        //let mOrderItems = [];
+        let mOrders = [];
+        let mTableID = -1
+        console.log("getOrders", ": ", "starting query...")
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            console.log("getOrders", ": ", "document found!")
+            console.log("current doc", doc);
+            //this.state.orders.push(doc.get("orderItems"))
+            //this.setState(test = doc.get("orderItems"))
+
+            let temp = doc.get("orderItems")
+            mTableID = [doc.get("tableID")]
+            mOrders.push(...temp)
+            mOrders.push("===================================")
+            //mOrders = mOrders.concat({tableID:mTableID,
+            //                        orderItems: mOrderItems})
+        });
+        console.log("mOrders", mOrders);
+        this.setState({
+            orders: mOrders
+        })
+    }
+
+    handleItemClick = (i) => {
+        let mOrders = this.state.orders
+        mOrders.splice(i, 1); // remove i
+        for(var j = 1; j < mOrders.length; j++) {
+            if(mOrders[j-1] === "===================================" && mOrders[j] === "===================================") {
+                mOrders.splice(j, 1);
+                mOrders.splice(j-1, 1);
+            }
+        }
+
+        this.setState({
+            orders: mOrders
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <button id="refreshButton"
+                        onClick={() => this.handleRefreshClick()}
+                >Refresh
+                </button>
+                <ul>
+                    {this.state.orders.map((order, i) =>
+                        <li key={i} onClick={() => this.handleItemClick(i)}> {order}</li>)}
+                    {/*<li key={i}><OrdersToMakeSubView*/}
+                    {/*            orderProps={order}/></li>)}*/}
+                </ul>
+            </div>
+
         )
     }
 
 }
+
 export default OrdersToMakeView
